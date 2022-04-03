94f1862f51a45dcfcedcde289e08aa8aac3a25d3
Dhruv Harsora <84675572+DhruvHarsora-FullStackDeveloper@users.noreply.github.com>
2022-04-03 08:04:42 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,6 +21,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server Started on: http://localhost:${port}`);
 });
